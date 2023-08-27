1c79f78cb9c7b21b2cb4a86fa3cc4de454b9b713
Luis Freyle <35880371+mariofreyle@users.noreply.github.com>
2023-08-27 13:42:41 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+import http from 'http';
+import url from 'url';
+import pathmodule from 'path';
+import fetch from 'node-fetch';
+
+const hostname = 'localhost';
+const port = 3000;
+
+const edges = {
+    'BOG' : '13.227.5.86',
+    'LIM' : '108.158.102.122',
+    'QRO' : '3.161.10.56',
+    'SCL' : '3.162.198.91',
+    'EZE' : '18.65.46.55',
+    'NBO' : '52.84.97.16',
+    'BAH' : '18.66.153.31',
+    'DXB' : '18.161.66.30',
+    'FJR' : '13.35.169.84',
+    'MCT' : '18.64.142.204'
+}
+
+/*const edges = {
+    'BOG' : '13.227.5.86',
+    'SCL' : '3.162.198.91'
+}*/
+
+function random(){
+    return ("000000000000000000" + Math.random().toString().slice(2)).slice(-12)
+}
+
+const server = http.createServer(async (req, res) => {
+    
+    let content = '',
+        response,
+        length;
+
+    async function req(ip){
+    return await fetch('http://' + ip + '/downloadx' + '?v=' + random(), {
+        method: 'GET',
+            headers: {
+              'Host': 'd375c8n0f70a17.cloudfront.net',
+            },
+        })
+    }
+    
+    for(let prop in edges){
+        response = await req(edges[prop])
+        if(response.ok){
+            length = (await response.text()).length
+        }
+        content += prop + ': refreshed ' + (response.ok ? ('✓' + ' (' + length + ')') : '✖')
+        content += "\n"
+        if(response.ok){
+            for(const pair of response.headers.entries()){
+                if(!pair[0].match(/cache-control|server|server-timing|timing-allow-origin|vary|via|x-amz-cf-id/)){
+                    content += `  ${pair[0]}: ${pair[1]}`
+                    content += "\n";
+                }
+            }
+        }
+        content += "\n"
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
+    res.end(content);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
